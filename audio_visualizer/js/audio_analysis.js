aa9dd74f031642db73b904f08bdf075659e62493
grant-tm <grant.t.morgan@gmail.com>
2024-02-17 19:17:10 -0800

    add rate control, time & frequency smoothing

--- audio_visualizer/js/audio_analysis.js
+++ audio_visualizer/js/audio_analysis.js
@@ -6,7 +6,7 @@ const audioSource = audioContext.createMediaElementSource(audioElement);
 class FFT {
     constructor(audioContext, audioSource, parameters = {}){
         const defaults = {
-            update: 100,
+            update_interval: 50,
             fft_size: 512,
             time_smoothing: 0.5,
             freq_smoothing: 0.5,
@@ -28,14 +28,31 @@ class FFT {
 
         this.data_frames = [];
         this.frame = new Uint8Array(this.parameters.fft_size / 2); 
+
+        this.interval_id = null;
+
+        this.max_smoothing_iterations = 10;
+    }
+
+    
+    smoothFreqs(data){
+        for(var j=0; j<(this.max_smoothing_iterations * this.freq_smoothing); j++){
+            for(var i=1; i<data.length-1; i++){
+                data[i].value = (data[i-1].value + data[i].value + data[i+1].value) / 3;
+            }
+        }
+        return data;
     }
 
     performFFT() {
-        setInterval(() => {
+        if(this.interval_id !== null){
+            clearInterval(this.interval_id);
+        }
+        this.interval_id = setInterval(() => {
             // Retrieve FFT data
             this.analyzer.getByteFrequencyData(this.frame);
             this.storeFFT(this.frame);
-        }, this.parameters.update_interval);
+        }, this.update_interval);
     }
 
     storeFFT(fft_data) {
@@ -50,11 +67,19 @@ class FFT {
         }
         new_frame.push({index: fft_data.length-1, value:0});
 
+        // smooth current frame
+        new_frame = this.smoothFreqs(new_frame);
+
         // add current frame to history
         if(this.data_frames.length > this.parameters.history_length){
             this.data_frames.shift();
         }
         this.data_frames.push(new_frame);
+    }
+
+    setTimeSmoothing(smoothing){
+        this.parameters.time_smoothing = smoothing;
+        this.analyzer.smoothingTimeConstant = smoothing;
     }
 
 }
